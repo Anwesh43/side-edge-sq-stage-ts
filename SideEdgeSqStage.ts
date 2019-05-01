46e6b9d7b758848a1397ccdd9589d0aafc0624b8
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 18:11:26 +0530

    adding DrawingUtil class

--- SideEdgeSqStage.ts
+++ SideEdgeSqStage.ts
@@ -6,6 +6,9 @@ const strokeFactor : number = 90
 const sizeFactor : number = 2.9
 const foreColor : string = "#673AB7"
 const backColor : string = "#BDBDBD"
+const eqFactor : number = 4
+const parts : number = 2
+const nodes : number = 5
 
 class ScaleUtil {
 
@@ -30,6 +33,43 @@ class ScaleUtil {
         return ScaleUtil.mirrorValue(scale, a, b) * dir * scGap
     }
 
+}
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, x2 : number, y1 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawSideEdgeLine(context : CanvasRenderingContext2D, sc1 : number, sc2 : number, size : number) {
+        const edgeSize : number = size / eqFactor
+        const position = -edgeSize + (-size + edgeSize) * sc1
+        for(var i = 0; i < parts; i++) {
+            context.save()
+            context.translate(position, position)
+            context.rotate(Math.PI/4 * (1 - 2 * i) * ScaleUtil.divideScale(sc2, i, parts))
+            DrawingUtil.drawLine(context, 0, 0, edgeSize, edgeSize)
+            context.restore()
+        }
+    }
+
+    static drawSESNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        const gap : number = w / (nodes + 1)
+        const size : number = gap / sizeFactor
+        context.strokeStyle = foreColor
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, 2)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, 2)
+
+        for (var j = 0; j < parts; j++) {
+            const sc1j : number = ScaleUtil.divideScale(scale, j, parts)
+            DrawingUtil.drawSideEdgeLine(context, sc1, sc2, size)
+        }
+    }
 }
 
 class SideEdgeSqStage {
