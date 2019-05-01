3d1e02afe5a7a41b912b01a11ad1bc51d25492d7
Anwesh43 <anweshthecool0@gmail.com>
2019-05-01 18:41:57 +0530

    adding SideEdgeEq class

--- SideEdgeSqStage.ts
+++ SideEdgeSqStage.ts
@@ -191,3 +191,27 @@ class SESNode {
         return this
     }
 }
+
+class SideEdgeEq {
+
+    root : SESNode = new SESNode(0)
+    curr : SESNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
